01543ecff6748eb858f65b1cbee8e6bfcd676f12
Hasan Tokatlı <hasantktl@gmail.com>
2013-11-17 12:23:34 +0200

    jquery kutuphanesi eklendi

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+</head>
+<body>
+<div>
+    
+    
+    
+    
+</div>
+<script src="js/jquery-1.10.2.js"></script>
+<script>
+
+</script>
+</body>
+</html>
